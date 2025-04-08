a3b4dbe26c7bd5976387ccab240432ff8373b54c
Bagus Hardiyanto <bagushardiyanto03@gmail.com>
2025-04-08 07:25:26 +0700

    menambahkan DataMahasiswa_103022300025

---
+++ modul7_kelompok_2/DataMahasiswa_103022300025.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+public class DataMahasiswa_103022300025
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Gender { get; set; }
+    public int Age { get; set; }
+    public Address Address { get; set; }          
+    public List<Course> Courses { get; set; }  
+    
+    public static DataMahasiswa_103022300025 ReadJSON(string filePath)
+    {
+        string json = File.ReadAllText(filePath);
+        return JsonConvert.DeserializeObject<DataMahasiswa_103022300025>(json);
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine($"Name: {FirstName} {LastName}");
+        Console.WriteLine($"Gender: {Gender}");
+        Console.WriteLine($"Age: {Age}");
+        Console.WriteLine($"Address: {Address.StreetAddress}, {Address.City}, {Address.State}");
+        Console.WriteLine("Courses:");
+        foreach (var course in Courses)
+        {
+            Console.WriteLine($"  - {course.Code}: {course.Name}");
+        }
+    }
+}
+
+// === Diletakkan di luar class utama ===
+public class Address
+{
+    public string StreetAddress { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+}
+
+public class Course
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
--- modul7_kelompok_2/Program.cs
+++ modul7_kelompok_2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        var data = DataMahasiswa_103022300025.ReadJSON(@"jurnal7_1_103022300025.json");
+        data.PrintData();
+    }
+}
